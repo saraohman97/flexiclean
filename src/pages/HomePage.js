91bc37bcac1f197265f862875b1d8f3c8edc01a3
Sara Öhman <saraohman97@gmail.com>
2023-02-07 09:49:07 +0100

    Small changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,8 @@ const HomePage = () => {
       <div className="home-header">
         <div className="container">
           <div className="home-text">
-            <h1>Våra filter räddar framtiden</h1>
+            {/* <h1>Våra filter räddar framtiden</h1> */}
+            <h1>Det finns inga önskebrunnar. Men väl en lyckosam lösning.</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum earum officiis ad neque unde molestias dicta ex commodi fugit eligendi! Libero inventore perferendis enim minus fugit nulla consectetur asperiores delectus unde! Nihil aliquid ducimus pariatur iste doloribus animi veniam? Error.</p>
           </div>
       </div>
