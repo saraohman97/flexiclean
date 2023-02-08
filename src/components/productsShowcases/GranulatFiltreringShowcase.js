9611520f590e26ef36535e440095170319d81b30
Sara Öhman <saraohman97@gmail.com>
2023-02-08 23:03:04 +0100

    added products on products page

---
+++ src/components/productsShowcases/GranulatFiltreringShowcase.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const GranulatFiltreringShowcase = () => {
+    return (
+        <div className='showcase-granulat'>
+            <div className='d-flex-a'>
+                <div className='gap'>
+                    <div>
+                        <h3>FlexiCleans Granulatkassetter för dagvattenrening och Granulatfiltrering (fotbollskassetten)</h3>
+                        <p>FlexiCleans granulatfilter för rening av dagvatten samt filtrering av granulat renar tungmetaller, näringsämnen, oljor, PAH och Pfas, samt filtrerar partiklar ned till 42 mikrometer. Kassetten rymmer 45 l/min filtrerat vatten. Filterpåsen som består av en blandning av furubark och träflis byts normalt en gång per år.</p>
+                    </div>
+                    <div>
+                        <h4>Montage</h4>
+                        <p>Att tänka på vid montaget är att filterkassetten kräver minst 8000/1000 mm djup i brunnen och med slät innersida på brunnen. Självaste "hålet" i beteckning måste vara 330 mm i diameter. Dessutom kan teleskopbeteckning försvåra montaget. </p>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Artikelnummer</th>
+                                <th>Passar till brunn</th>
+                                <th>Filtertyp</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>200100 FC 408 Granulat</td>
+                                <td>350-450mm</td>
+                                <td>Bark/flis</td>
+                            </tr>
+                            <tr>
+                                <td>200102 FC 508 Granulat</td>
+                                <td>500 mm</td>
+                                <td>Bark/flis</td>
+                            </tr>
+                            <tr>
+                                <td>200103 FC 608 Granulat</td>
+                                <td>600 mm</td>
+                                <td>Bark/flis</td>
+                            </tr>
+                            <tr>
+                                <td>200104 FC 1008 Granulat</td>
+                                <td>1000 mm</td>
+                                <td>Bark/flis</td>
+                            </tr>
+                            <br />
+                            <tr>
+                                <td>100113 Filterdyna 600mm</td>
+                                <td>600 mm</td>
+                                <td>Bark/flis</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <img src="https://h24-original.s3.amazonaws.com/252829/29670684-zGYJd.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default GranulatFiltreringShowcase
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,8 +1,113 @@
 import React from 'react'
+import DagvattenReningShowcase from '../components/productsShowcases/DagvattenReningShowcase'
+import GranulatFiltreringShowcase from '../components/productsShowcases/GranulatFiltreringShowcase'
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className='products-page'>
+      <div className="container">
+
+        <h2>Våra filter</h2>
+
+
+        <DagvattenReningShowcase />
+
+        <GranulatFiltreringShowcase />
+
+        <div className="showcase-wall">
+          <div className='d-flex'>
+            <div>
+              <h3>FlexiClean filterväggar för dagvattenrening</h3>
+              <p>FlexiClean filterväggar för rening av dagvatten. Filtret renar tungmetaller, näringsämnen, oljor PAH och Pfas. Och är godkänd som oljeavskiljare på parkeringsplatser. Flödet beror på hur många kassetter man använder.Filtren är försedda med en bypassfunktion vid höga flöden. Filterpåsen som består av en blandning av furubark och träflis byts normalt en gång per år.
+
+                Kontakta oss för närmare information.</p>
+            </div>
+
+            <img src="https://h24-original.s3.amazonaws.com/252829/29671418-qZqDq.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="showcase-brunn">
+          <div className='d-flex'>
+            <div className='gap'>
+              <h3>FlexiClean filterbrunnar för dagvattenrening</h3>
+              <p>
+                FlexiClean filterbrunnar för rening av dagvatten. Dessa är för permanent placering men kan även användas mobilt. Filtret renar tungmetaller, näringsämnen, oljor PAH och Pfas. Filterbrunnen är fördelad på en sedimenteringsdel samt en filtreringsdel . Filterbrunnen flödar 320-640 l/min filtrerat vatten beroende på dimension Filtren är försedda med en bypassfunktion vid höga flöden. Filterpåsen som består av en blandning av furubark och träflis byts normalt en gång per år.
+              </p>
+              <p>
+                Kontakta oss för närmare information.
+              </p>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th>Artikelnummer</th>
+                    <th>Antal filterkassetter</th>
+                    <th>Annat</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>79 122 Filterbrunn Dagvatten DN600</td>
+                    <td>2 st</td>
+                    <td>FlexiClean 408, 19 m3/h</td>
+                  </tr>
+                  <tr>
+                    <td>79 123 Filterbrunn Dagvatten DN1000</td>
+                    <td>3 st</td>
+                    <td>FlexiClean 408, 29 m3/h</td>
+                  </tr>
+                  <tr>
+                    <td>79 124 Filterbrunn Dagvatten DN1200</td>
+                    <td>4 st</td>
+                    <td>FlexiClean 408, 38 m3/h</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <img src="https://h24-original.s3.amazonaws.com/252829/29670696-DJXfM.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="showcase-clean">
+          <div className='d-flex'>
+            <div>
+              <h3>FlexiCleans Målartvätt</h3>
+              <p>FlexiClean målartvätt. En enkel icke mekanisk filtrering som kopplas till befintligt avlopp under diskbänken. Filtret renar tungmetaller, näringsämnen, oljor PAH och Pfas.
+                Kontakta oss för närmare information.</p>
+            </div>
+
+            <img src="https://h24-original.s3.amazonaws.com/252829/30249150-teMDG.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="showcase-clean">
+          <div className='d-flex'>
+            <div>
+              <h3>FlexiCleans Brunnslock</h3>
+              <p>FlexiClean brunnslock. Förhindra effektivt större fragment att hamna i dagvattenbrunnen.
+                Kontakta oss för närmare information.</p>
+            </div>
+
+            <img src="https://h24-original.s3.amazonaws.com/252829/30249151-LghYS.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="showcase-clean">
+          <div className='d-flex'>
+            <div>
+              <h3>FlexiCleans Filterkorg</h3>
+              <p>FlexiClean filterkorg. Förhindrar effektivt större fragment att hamna i dagvattnet. Korgen placeras i dagvattenbrunnen.
+                Kontakta oss för närmare information.</p>
+            </div>
+
+            <img src="https://h24-original.s3.amazonaws.com/252829/30249156-WiNiq.png" alt="" />
+          </div>
+        </div>
+
+      </div>
+    </div >
   )
 }
 
