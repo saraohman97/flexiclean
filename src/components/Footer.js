4bb67aab9e49f45206b618267e7b1d5ea406e719
Sara Öhman <saraohman97@gmail.com>
2023-07-13 00:45:44 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,20 @@ const Footer = () => {
   const { currentUser } = useContext(AuthContext)
 
   return (
-    <div className='footer'>
-      <div className="footer-links">
-        <Link to='/' className='footer-link'>Hem</Link>
-        <Link to='/produkter' className='footer-link'>Produkter</Link>
-        <Link to='/kontakta-oss' className='footer-link'>Kontakta oss</Link>
-        <Link to={currentUser ? '/skapa' : '/admin'} className='footer-link'>Admin</Link>
+    <div className='absolute bottom-0 w-full bg-gray-100 p-10 flex flex-col gap-2 justify-center items-center'>
+      <div className="flex items-center gap-6">
+        <Link to='/' className='hover:text-blue-400'>Hem</Link>
+        <Link to='/produkter' className='hover:text-blue-400'>Produkter</Link>
+        <Link to='/kontakta-oss' className='hover:text-blue-400'>Kontakta oss</Link>
+        <Link to={currentUser ? '/skapa' : '/admin'} className='hover:text-blue-400'>Admin</Link>
       </div>
 
-      <small className='copyright'>Copyright © 2015</small>
+      <small className='text-gray-400'>Copyright © 2015</small>
 
-      <div className="footer-icons">
-        <a href="https://twitter.com/FlexiClean" className='footer-icon'><AiOutlineTwitter /></a>
-        <a href="https://www.linkedin.com/in/peder-eneroth-6543a366/" className='footer-icon'><AiFillLinkedin /></a>
-        <a href="https://www.youtube.com/@FlexiClean" className='footer-icon'><AiFillYoutube /></a>
+      <div className="flex items-center gap-6">
+        <a href="https://twitter.com/FlexiClean" className='text-2xl hover:text-blue-400'><AiOutlineTwitter /></a>
+        <a href="https://www.linkedin.com/in/peder-eneroth-6543a366/" className='text-2xl hover:text-blue-400'><AiFillLinkedin /></a>
+        <a href="https://www.youtube.com/@FlexiClean" className='text-2xl hover:text-blue-400'><AiFillYoutube /></a>
       </div>
     </div>
   )
